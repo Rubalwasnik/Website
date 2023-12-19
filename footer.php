c3d41fa376edf991e63de4208c87529ab3426506
Rubalwasnik <139051027+Rubalwasnik@users.noreply.github.com>
2023-12-19 15:22:43 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+
+?>
+
+<footer>
+        <section>
+            <figure><img src="./img/modest life logo new.png" alt="" height="60px"></figure>
+            <figcaption>Modest Life</figcaption>
+        </section>
+        <section class="footer_text">
+            <h2>Social</h2>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </section>
+        <section class="footer_text">
+            <h2>Important Link</h2>
+            <ul>
+                <li><a href="#">Terms and Conditions</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Disclaimer</a></li>
+            </ul>
+        </section>
+    </footer>
+</body>
+</html>
